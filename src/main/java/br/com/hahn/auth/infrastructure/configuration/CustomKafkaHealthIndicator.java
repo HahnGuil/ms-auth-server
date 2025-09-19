f2b3fa60c45b291e8f0b1910b2c10993be5c791c
hahnguil <guilherme.f.h@hotmail.com>
2025-09-18 23:27:47 -0300

    feat: update Kafka configuration to use dynamic bootstrap servers and consumer settings

--- src/main/java/br/com/hahn/auth/infrastructure/configuration/CustomKafkaHealthIndicator.java
+++ src/main/java/br/com/hahn/auth/infrastructure/configuration/CustomKafkaHealthIndicator.java
@@ -2,6 +2,7 @@ package br.com.hahn.auth.infrastructure.configuration;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,13 @@ import java.util.concurrent.ExecutionException;
 @Component
 public class CustomKafkaHealthIndicator implements HealthIndicator {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Override
     public Health health() {
         Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         try (AdminClient client = AdminClient.create(props)) {
             client.listTopics().names().get();
             return Health.up().withDetail("kafka", "Available").build();
--- src/main/java/br/com/hahn/auth/infrastructure/configuration/KafkaConfig.java
+++ src/main/java/br/com/hahn/auth/infrastructure/configuration/KafkaConfig.java
@@ -2,6 +2,7 @@ package br.com.hahn.auth.infrastructure.configuration;
 
 import br.com.hahn.auth.domain.model.UserSyncEvent;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -15,15 +16,28 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
+
+
     @Bean
     public ConsumerFactory<String, UserSyncEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "update-user-data");
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("group.id", groupId);
         props.put("key.deserializer", StringDeserializer.class);
         props.put("value.deserializer", JsonDeserializer.class);
-        props.put("spring.kafka.consumer.auto-offset-reset", "earliest");
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "br.com.hahn.auth.application.dto, br.com.hahn.auth.domain.model");
+        props.put("auto.offset.reset", autoOffsetReset);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "br.com.hahn.auth.application.dto,br.com.hahn.auth.domain.model");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "br.com.hahn.auth.domain.model.UserSyncEvent");
 
         return new DefaultKafkaConsumerFactory<>(
                 props,
@@ -38,6 +52,8 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, UserSyncEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
+
         return factory;
     }
+
 }
