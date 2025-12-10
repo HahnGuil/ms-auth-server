02e25a357214cbb622973dc6e01b6aabb894fb5e
hahnguil <guilherme.f.h@hotmail.com>
2025-12-10 13:50:47 -0300

    refact: Remove unused import for AllArgsConstructor in UserSyncService

--- src/main/java/br/com/hahn/auth/application/service/UserSyncService.java
+++ src/main/java/br/com/hahn/auth/application/service/UserSyncService.java
@@ -1,6 +1,5 @@
 package br.com.hahn.auth.application.service;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
